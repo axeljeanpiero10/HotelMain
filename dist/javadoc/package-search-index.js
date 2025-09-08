2ef88e5fe0a67c4b775d95967f370facba7c9dd9
Axel Jean Piero Bazan Ramos <71866269+axeljeanpiero10@users.noreply.github.com>
2025-09-08 17:45:49 -0500

    Add files via upload

---
+++ dist/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.Conexion"},{"l":"com.Modelo"}];updateSearchResults();
---
+++ dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.Conexion","l":"Conexion"},{"p":"com.Modelo","l":"usuario"},{"p":"com.Modelo","l":"usuarioDAO"}];updateSearchResults();
